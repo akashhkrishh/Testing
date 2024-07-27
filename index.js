0706f902d2ae80456a26756917c70700af10180f
Akashh Krishh <akashhkrishh@gmail.com>
2024-07-27 11:17:57 +0530

    .

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
 app.post("/",(req,res)=>{
     try {
         const { name } = req.body;
-        
+         
         res.send(200,`Hello ${name}`);
     } catch (error) {
+        console.log(error)
         res.send(500, error);
     }
     
