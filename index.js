673f6091f3888ebdc0f4f0ad5c98c36c17e84185
Akashh Krishh <akashhkrishh@gmail.com>
2024-07-27 11:09:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res)=>{
+app.post("/",(req,res)=>{
     try {
         const { name } = req.body;
+        
         res.send(200,`Hello ${name}`);
     } catch (error) {
         res.send(500,"Internal Server Error");
